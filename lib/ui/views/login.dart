dc28ef4d65d1c06f7aa587b6991e098a82dc034e
Hasini Madhuwanthi <33367240+SayalaGalvin@users.noreply.github.com>
2020-10-18 19:23:41 +0530

    Update login.dart

--- lib/ui/views/login.dart
+++ lib/ui/views/login.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-/*class LoginPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-      return Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: Column(
-              
-          ),
-        ),
-      );
-  }
-*/
 class LoginPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => new _LoginPageState();
