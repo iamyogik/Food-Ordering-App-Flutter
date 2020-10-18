8c56e3d7654da76b3f4d9df387003cf168811373
Hasini Madhuwanthi <33367240+SayalaGalvin@users.noreply.github.com>
2020-10-18 20:30:15 +0530

    Update login_model.dart

--- lib/core/viewmodels/login_model.dart
+++ lib/core/viewmodels/login_model.dart
@@ -3,7 +3,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import '../services/localstorage_service.dart';
 import './base_model.dart';
-import '../../ui/views/login.dart';
 import '../../locator.dart';
 
 class LoginModel extends BaseModel {
